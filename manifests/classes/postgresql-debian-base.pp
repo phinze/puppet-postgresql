0633e8d911d1c32c837f06b1f940c02b368cab62
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-05-12 08:06:26 +0200

    postgresql: missing dependency

--- manifests/classes/postgresql-debian-base.pp
+++ manifests/classes/postgresql-debian-base.pp
@@ -30,16 +30,18 @@ class postgresql::debian::base inherits postgresql::base {
   exec {"drop initial cluster":
     command     => "pg_dropcluster --stop ${version} main",
     refreshonly => true,
+    onlyif      => "test \$(su -c \"psql -tA -c 'SELECT count(*)=3 FROM pg_catalog.pg_database;'\" postgres) = t",
     timeout     => 60,
   }
 
+  
   postgresql::cluster {"main":
     ensure      => present,
     clustername => "main",
     version     => $version,
     encoding    => "UTF8",
     data_dir    => "${postgresql::params::data_dir}",
-    require     => Package["postgresql"],
+    require     => [Package["postgresql"], Exec["drop initial cluster"]],
   }
   
 }
